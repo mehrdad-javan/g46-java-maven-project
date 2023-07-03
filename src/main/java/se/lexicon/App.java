98865005e46b315c54f8e0fca83856ec0eee61f6
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-07-03 10:46:34 +0200

    part 2: variables

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -12,6 +12,35 @@ public class App
     // this is the main method - not allow to change it
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        byte redColorCode = 127;
+        byte greenColorCode = -128;
+        byte blueColorCode = 0;
+
+        short temperature = -300;
+        short groupId = 46;
+
+        int salary = 600000;
+        int tax = 450000;
+
+        long bankBalance = 999999999999999L;
+
+        float PIE = 3.14f;
+        float GRAVITY = 9.8F;
+
+        boolean studentStatus = true;
+
+        char a = 'a';
+        char testCharA = 'A';
+
+        char hashtag = '\u0023';
+        char omega = '\u03A9';
+
+
+        System.out.println(redColorCode);
+        System.out.println(studentStatus);
+        System.out.println(omega);
+
     }
 
 
