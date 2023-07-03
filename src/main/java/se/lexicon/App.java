9ef13e5a536610bbe7a7e017e7dd552e1bd4eefd
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-07-03 09:49:19 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,21 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    /*
+        this method will print
+        hello world text to the console
+     */
+
+    // this is the main method - not allow to change it
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+
+}
